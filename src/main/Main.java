b274a16f8a39798ffbb1f931dd3c36e71e267291
Emmanuel <kcemmy03@gmail.com>
2025-06-14 16:04:41 +0100

    Entry point to java program

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println(" Welcome to Java Expense Tracker ");
+        System.out.println("=================================");
+        System.out.println("Let's help you manage your finances like a pro");
+    }
+}
